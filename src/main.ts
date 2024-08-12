0c283c7cbabcd6022d7ed216408b53d265fcbdbb
amalej <alejandroalejandre15@gmail.com>
2024-08-12 16:09:12 +0800

    Format and prep for release
    - Fix issue with vite build - Success
    - Attempt to make output smaller - Failed

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { OhVueIcon } from "oh-vue-icons";
-import { addIcons } from "oh-vue-icons";
-import { BiTrophyFill } from "oh-vue-icons/icons";
-import { FaPlay } from "oh-vue-icons/icons";
-import { MdArrowbackRound } from "oh-vue-icons/icons";
-import { LaGlobeSolid } from "oh-vue-icons/icons";
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import {
+  BiTrophyFill,
+  FaPlay,
+  MdArrowbackRound,
+  LaGlobeSolid,
+} from "oh-vue-icons/icons";
 
 addIcons(BiTrophyFill, FaPlay, MdArrowbackRound, LaGlobeSolid);
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  /** Splits the js file into multiple files when uncommented */
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes("node_modules")) {
+  //           return id
+  //             .toString()
+  //             .split("node_modules/")[1]
+  //             .split("/")[0]
+  //             .toString();
+  //         }
+  //       },
+  //     },
+  //   },
+  // },
+  /** Removes legal comments reducing size when uncommented */
+  // esbuild: { legalComments: "none" },
+});
